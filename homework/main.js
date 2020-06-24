e009332280def70ced0b954b582853e5324a1656
S_Kurochkin <idsemen1992@ya.ru>
2020-06-24 21:05:50 +0300

    DOM

--- homework/main.js
+++ homework/main.js
@@ -1,74 +1,75 @@
-'use strict'
-
-
-let numberOfFilms;
-
-
-
-
-start();
-
+let numberOfFilms = '';
 let personalMovieDB = {
-	count: numberOfFilms,
+	count: '',
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
-};
-
-rememberMyFilms();
-checkCountFilms();
-writeYourGenres();
-showMyDB();
-
-
-function start() {
-	numberOfFilms = +prompt('Сколько фильмов вы уже посморели?');
-
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+	privat: false,
+	start: function () { 
+		let numberOfFilms;
 		numberOfFilms = +prompt('Сколько фильмов вы уже посморели?');
-	}
-}
 
-function rememberMyFilms() {
-	for (let i = 0; i < 2; i++) {
-		let a = prompt('Один из последних фильмов');
-		let b = +prompt('Оценка');
-		if (a == null || a.length > 50 || a == '' || b == null || b == '') {
-			console.log('Error');
-			i--;
-		} else {
-			console.log('Done');
-			personalMovieDB.movies[a] = b;
+		while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+			numberOfFilms = +prompt('Сколько фильмов вы уже посморели?');
 		}
+		return personalMovieDB.count = numberOfFilms;
+	},
+	rememberMyFilms: function () {
+		for (let i = 0; i < 2; i++) {
+			let a = prompt('Один из последних фильмов');
+			let b = +prompt('Оценка');
+			if (a == null || a.length > 50 || a == '' || b == null || b == '') {
+				console.log('Error');
+				i--;
+			} else {
+				console.log('Done');
+				personalMovieDB.movies[a] = b;
+			}
 
-	}
+		}
+	},
+	checkCountFilms: function () {
+		if (personalMovieDB.count < 10) {
+			alert('Просмотрено мало фильмов')
+		} else if (personalMovieDB.count < 30) {
+			alert('Вы классический зритель')
+		} else {
+			alert('Вы - киноман')
+		}
+	},
+
+	writeYourGenres: function () {
+		for (let i = 1; i < 4; i++) {
+			let a = prompt(`Ваш любимый жанр под номером ${i}`);
+			if (a == null || a.length > 50 || a == '') {
+				console.log('Error');
+				i--;
+			} else {
+				personalMovieDB.genres[i - 1] = a;
+			}
+		}
+		personalMovieDB.genres.forEach(function (elem, index) {
+			console.log(`Любимый жанр #${index+1} - это ${elem}`);
+		});
+
+
+	},
+	showMyDB: function () {
+		if (personalMovieDB.privat == false)
+			console.log(personalMovieDB);
+	},
+
+	toggleVisibleDB: function () {
+		if (personalMovieDB.privat == false) {
+			personalMovieDB = true;
+		} else {
+			personalMovieDB = false;
+		}
+	},
+
 }
-
-
-function checkCountFilms() {
-	if (personalMovieDB.count < 10) {
-		alert('Просмотрено мало фильмов')
-	} else if (personalMovieDB.count < 30) {
-		alert('Вы классический зритель')
-	} else {
-		alert('Вы - киноман')
-	}
-}
-
-
-function writeYourGenres(){
-	for(let i=1; i<4; i++){
-
-		personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-	}
-}
-
-
-function showMyDB(){
-	if(personalMovieDB.privat == false)
-		console.log(personalMovieDB);
-}
-
-
-
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.checkCountFilms();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
---
+++ homework/script.js
@@ -0,0 +1,123 @@
+// const arr = [23, 33, 2, 767, 1, 10, -1000, 19, 45, 8, 2, ];
+// const b = ['a', 'b', 'c'];
+
+// arr.splice(1, 2);
+// console.log(arr);
+
+// let sum = 0;
+// let max = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//     if (max < arr[i]) {
+//         max = arr[i];
+//     }
+// }
+// console.log('sum - ' + sum);
+// console.log('max - '+ max);
+
+/* let c = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+let d = [];
+for (let i = 0; i < c.length; i++){
+    // console.log(c[i]);
+    for(let k = 0; k<c[i].length; k++){
+        if(c[i][k] > 4)
+        d.push(c[i][k]);
+    }
+}
+console.log(d); */
+
+// let a = [1, 0, 0, 3, 48, 0];
+// let k = 0;
+// document.querySelector('div').innerHTML = a;
+// document.querySelector('button').onclick = () => {
+//     if (k + 1 < a.length) {
+//         a[k] = 0;
+//         a[k + 1] = 1;
+//         k++;
+//     } else {
+//         a[k] = 0;
+//         k = 0;
+//         a[k] = 1;
+//     }
+
+//     document.querySelector('div').innerHTML = a;
+// };
+
+
+// const a = {
+//     'a': 5,
+//     'b': 'Hello',
+//     z2: 19999
+// };
+
+// console.log();
+
+
+
+
+// let a = new Set();
+// a.add(2);
+// a.add(4);
+// a.add(8);
+// a.add('ready');
+
+// for (let set of a) {
+//     console.log(set);
+// };
+
+
+// let b = [1, 201, 3, 4, 3, 21, 'hello', 4, 5, 15, 'world'];
+// let c = new Set(b);
+// let d =[];
+// c.forEach(function(value){
+//     console.log(value + ' есть в коллекции');
+// });
+
+// let a = [1, 202, 3, 4, 3, 21, 4, 5, 15, 'fox', 'rex'];
+
+// let b = a.map(function(elem, index){
+//     console.log(elem);
+//     return elem + 5;
+// });
+
+
+// b = a.filter(elem => elem == 8);
+// console.log(b);
+
+// const video = [1, [2,3], 4],
+//     blog = [5,6,7];
+
+// // const newarr = [...video, ...blog];
+// const copyArr = [...blog];
+
+// copyArr[1] = 100;
+// console.log(copyArr);
+// console.log(blog);
+
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+
+// }
+
+// const num = [2,3,5,4];
+
+// log(...num);
+
+// let y = 1;
+// let x = y = 2;
+
+// console.log(x);
+//  let a = [1,2,3];
+//  let b = [1,2,3];
+
+// console.log('rEx'> 'rex');
+// let d = document.querySelector('[data="go"]');
+// console.log(d);
